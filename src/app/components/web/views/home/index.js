1ea9eafcd248ba67637ea139cf08d6d8fd5da17e
l.tessore <ltessore@ltessore>
2022-04-19 15:41:02 -0300

    Imprime por consola el numero de mesa

--- src/app/components/web/views/home/index.js
+++ src/app/components/web/views/home/index.js
@@ -3,7 +3,17 @@ import Bannerslider from '../banner-carousel';
 import Bestofferbanner from './best-offers-banner';
 import Topstample from './top-stample';
 
+
 export default class Home extends Component {
+    
+    
+    constructor(props){
+        super(props)
+        const querystring = window.location.search
+        const params = new URLSearchParams(querystring);
+        console.log("El nùmero de mesa es : "+params.get('mesaId'));
+    }
+    
     render() {
         return (
             <div className="wrapper">
